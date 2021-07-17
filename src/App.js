8cce25d2923f6533d123f6103d09269c55cc2ad4
priyanshubansal <56711288+priyanahubansal@users.noreply.github.com>
2021-07-18 00:37:56 +0530

    'comit'

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ get_WeatherIcon(icons,rangeId){
 
 
     if(city && country){
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
   
     const response = await api_call.json();
 
